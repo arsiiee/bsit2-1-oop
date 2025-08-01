50c78b08bd29238eb36f8aba82d7ab7a1bc0cb6a
arsiiee <azsalcedo64143@liceo.edu.ph>
2025-08-01 11:18:08 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+import java.util.Scanner;
+
+public class inputNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int total = 0; 
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+            total += number;
+        }
+
+        // display total
+        System.out.println("\nTotal Number: " + total);
+
+        input.close();
     }
 }
