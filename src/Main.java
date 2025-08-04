a45d7651ed8fb4bc7f723fc2cc6271392f05fed1
arsiiee <azsalcedo64143@liceo.edu.ph>
2025-08-04 10:16:31 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+class Main {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
+
+    public Main(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book \"" + title + "\" by " + author + " has been added to the library!");
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("The book \"" + title + "\" has been borrowed.");
+        } else {
+            System.out.println("The book \"" + title + "\" is currently unavailable.");
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+        System.out.println("You have returned \"" + title + "\".");
+    }
+
+    public static void main(String[] args) {
+        Main book1 = new Main("Babel", "R.F. Kuang", 544);
+        Main book2 = new Main("The Hobbit", "J.R.R. Tolkien", 310);
+        Main book3 = new Main("1984", "George Orwell", 328);
+
+        System.out.println();
+        book1.displayInfo();
+        book1.borrowBook();
+        book1.displayInfo();
+        book1.returnBook();
+        book1.displayInfo();
+
+        System.out.println();
+        book2.displayInfo();
+        book2.borrowBook();
+        book2.displayInfo();
+        book2.returnBook();
+        book2.displayInfo();
+
+        System.out.println();
+        book3.displayInfo();
+        book3.borrowBook();
+        book3.displayInfo();
+        book3.returnBook();
+        book3.displayInfo();
     }
 }
